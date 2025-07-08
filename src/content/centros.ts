e4e4365a70dab41c825fbc1d58afa39b1c424ed2
turing44 <rgavhur120@g.educaand.es>
2025-07-08 16:39:04 +0200

    para presentacion

---
+++ src/content/centros.ts
@@ -0,0 +1,96 @@
+export interface Centro {
+    nombre: string,
+    direccion: string,
+    linkMaps: string,
+    imagen: string
+    horario: {
+        lunes: string,
+        martes: string,
+        miercoles: string,
+        jueves: string,
+        viernes: string,
+        sabado: string,
+        domingo: string
+    }
+};
+
+const listaCentros: Centro[] = [
+    {
+        nombre: "Alcosa",
+        direccion: "Calle alcosa de sevilla, 12",
+        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
+        imagen: "/public/uploads/centroAlcosa.webp",
+        horario: {
+            lunes: "12:00 - 18:00",
+            martes: "12:00 - 18:00",
+            miercoles: "12:00 - 18:00",
+            jueves: "12:00 - 18:00",
+            viernes: "12:00 - 18:00",
+            sabado: "Cerrado",
+            domingo: "Cerrado",
+        }
+    },
+    {
+        nombre: "Alcosa",
+        direccion: "Calle alcosa de sevilla, 12",
+        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
+        imagen: "/public/uploads/centroAlcosa.webp",
+        horario: {
+            lunes: "12:00 - 18:00",
+            martes: "12:00 - 18:00",
+            miercoles: "12:00 - 18:00",
+            jueves: "12:00 - 18:00",
+            viernes: "12:00 - 18:00",
+            sabado: "Cerrado",
+            domingo: "Cerrado",
+        }
+    },
+    {
+        nombre: "Alcosa",
+        direccion: "Calle alcosa de sevilla, 12",
+        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
+        imagen: "/public/uploads/centroAlcosa.webp",
+        horario: {
+            lunes: "12:00 - 18:00",
+            martes: "12:00 - 18:00",
+            miercoles: "12:00 - 18:00",
+            jueves: "12:00 - 18:00",
+            viernes: "12:00 - 18:00",
+            sabado: "Cerrado",
+            domingo: "Cerrado",
+        }
+    },
+    {
+        nombre: "Alcosa",
+        direccion: "Calle alcosa de sevilla, 12",
+        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
+        imagen: "/public/uploads/centroAlcosa.webp",
+        horario: {
+            lunes: "12:00 - 18:00",
+            martes: "12:00 - 18:00",
+            miercoles: "12:00 - 18:00",
+            jueves: "12:00 - 18:00",
+            viernes: "12:00 - 18:00",
+            sabado: "Cerrado",
+            domingo: "Cerrado",
+        }
+    },
+    {
+        nombre: "Alcosa",
+        direccion: "Calle alcosa de sevilla, 12",
+        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
+        imagen: "/public/uploads/centroAlcosa.webp",
+        horario: {
+            lunes: "12:00 - 18:00",
+            martes: "12:00 - 18:00",
+            miercoles: "12:00 - 18:00",
+            jueves: "12:00 - 18:00",
+            viernes: "12:00 - 18:00",
+            sabado: "Cerrado",
+            domingo: "Cerrado",
+        }
+    },
+    
+];
+
+export default listaCentros;

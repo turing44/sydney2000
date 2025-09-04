5ad49d653f61724c809feb7b16e5d5c2762f69ec
rober <rgavhur120@g.educaand.es>
2025-09-04 19:32:21 +0200

    Informacion real annadida, falta premios

--- src/content/centros.ts
+++ src/content/centros.ts
@@ -2,95 +2,56 @@ export interface Centro {
     nombre: string,
     direccion: string,
     linkMaps: string,
-    imagen: string
-    horario: {
-        lunes: string,
-        martes: string,
-        miercoles: string,
-        jueves: string,
-        viernes: string,
-        sabado: string,
-        domingo: string
-    }
+    imagen: string,
+    horario: Object
 };
 
 const listaCentros: Centro[] = [
     {
-        nombre: "Alcosa",
-        direccion: "Calle alcosa de sevilla, 12",
-        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
-        imagen: "/public/uploads/centroAlcosa.webp",
+        nombre: "Escuela San Luis",
+        direccion: "C\\ San Luis nº 29",
+        linkMaps: "https://maps.app.goo.gl/4P6nqXr4RiHjBtMp8",
+        imagen: "",
         horario: {
-            lunes: "12:00 - 18:00",
-            martes: "12:00 - 18:00",
-            miercoles: "12:00 - 18:00",
-            jueves: "12:00 - 18:00",
-            viernes: "12:00 - 18:00",
-            sabado: "Cerrado",
-            domingo: "Cerrado",
+            "Lunes": "16:00 - 17:30",
+            "Martes": "16:00 - 17:30",
+            "Miercoles": "16:00 - 17:30",
+            "Jueves": "16:00 - 17:30",
+            "Viernes": "16:00 - 18:00"
         }
     },
     {
-        nombre: "Alcosa",
-        direccion: "Calle alcosa de sevilla, 12",
-        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
-        imagen: "/public/uploads/centroAlcosa.webp",
+        nombre: "Escuela San Pablo",
+        direccion: `Av. Doctor Laffón Soto`,
+        linkMaps: "https://maps.app.goo.gl/WQXrsDgeRuALiPsM7",
+        imagen: "",
         horario: {
-            lunes: "12:00 - 18:00",
-            martes: "12:00 - 18:00",
-            miercoles: "12:00 - 18:00",
-            jueves: "12:00 - 18:00",
-            viernes: "12:00 - 18:00",
-            sabado: "Cerrado",
-            domingo: "Cerrado",
+            "Lunes": "16:00 - 17:30",
+            "Miercoles": "16:00 - 17:30",
         }
     },
     {
-        nombre: "Alcosa",
-        direccion: "Calle alcosa de sevilla, 12",
-        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
-        imagen: "/public/uploads/centroAlcosa.webp",
+        nombre: "Escuela Sevilla Este",
+        direccion: "Calle Cueva de la Pileta S/N",
+        linkMaps: "https://maps.app.goo.gl/mVUNz1TArSAL8YKu7",
+        imagen: "",
         horario: {
-            lunes: "12:00 - 18:00",
-            martes: "12:00 - 18:00",
-            miercoles: "12:00 - 18:00",
-            jueves: "12:00 - 18:00",
-            viernes: "12:00 - 18:00",
-            sabado: "Cerrado",
-            domingo: "Cerrado",
+            "Jueves": "16:00 - 18:00"
         }
     },
     {
-        nombre: "Alcosa",
-        direccion: "Calle alcosa de sevilla, 12",
-        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
-        imagen: "/public/uploads/centroAlcosa.webp",
+        nombre: "Escuela CEIP Arrayanes",
+        direccion: "Av. Ciudad de Chiva, 32",
+        linkMaps: "https://maps.app.goo.gl/3WAX7ZqC3rqt8Gu97",
+        imagen: "",
         horario: {
-            lunes: "12:00 - 18:00",
-            martes: "12:00 - 18:00",
-            miercoles: "12:00 - 18:00",
-            jueves: "12:00 - 18:00",
-            viernes: "12:00 - 18:00",
-            sabado: "Cerrado",
-            domingo: "Cerrado",
+            "Lunes Grupo 1": "16:00 - 17:30",
+            "Miercoles Grupo 1": "16:00 - 17:30",
+            "Lunes Grupo 2": "17:30 - 19:00",
+            "Miercoles Grupo 2": "17:30 - 19:00"
         }
     },
-    {
-        nombre: "Alcosa",
-        direccion: "Calle alcosa de sevilla, 12",
-        linkMaps: "https://maps.app.goo.gl/VJGGac28s9CBqcHY8",
-        imagen: "/public/uploads/centroAlcosa.webp",
-        horario: {
-            lunes: "12:00 - 18:00",
-            martes: "12:00 - 18:00",
-            miercoles: "12:00 - 18:00",
-            jueves: "12:00 - 18:00",
-            viernes: "12:00 - 18:00",
-            sabado: "Cerrado",
-            domingo: "Cerrado",
-        }
-    },
-    
+        
 ];
 
 export default listaCentros;
--- src/content/siteInfo.ts
+++ src/content/siteInfo.ts
@@ -1,7 +1,9 @@
 const siteInfo = {
     titulo: "C. D. Sydney 2000",
-    telf: "677 122 133",
+    telf: "660031399",
+    mail: "ritmicasydney2000@gmail.com",
     direccion: "Calle de la Amargura, 42",
     description: "Escuela de gimnasia ritmica con más de 20 años de experiencia",
+    horario_atencion: "Lunes a Viernes 10:00-13:30h 16:30-20:30h"
 }
 export default siteInfo;
